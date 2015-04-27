d195dd446461900baf7d7f2ec815cdac30208cb6
Kevin Yokley <kyokley2@gmail.com>
2015-04-26 20:36:27 -0500

    Adding ycm install

--- linkVim.sh
+++ linkVim.sh
@@ -13,6 +13,9 @@ sudo npm install -g jslint
 sudo npm install -g jshint
 sudo pip install flake8 yapf
 
+sudo aptitude install build-essential cmake python-dev
+~/.vim/bundle/YouCompleteMe/install.sh --clang-completer
+
 cd ~/.vim
 tar -zxvf rope-0.9.3.tar.gz
 cd rope-0.9.3
