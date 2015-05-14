22d1f5585bec3a05b8e3c9baa8f9f314e39863c1
Kevin Yokley <kyokley2@gmail.com>
2015-05-14 11:31:46 -0500

    Adding silversearcher to install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ fi
 
 ln -s $HOME/.vim/vimrc $HOME/.vimrc
 
-sudo aptitude install rake vim-nox ruby-dev exuberant-ctags ack-grep pyflakes pep8 nodejs npm
+sudo aptitude install rake vim-nox ruby-dev exuberant-ctags ack-grep pyflakes pep8 nodejs npm silversearcher-ag
 sudo npm config set registry http://registry.npmjs.org/
 sudo npm install -g jslint
 sudo npm install -g jshint
